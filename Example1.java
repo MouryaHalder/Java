38e6252e9543bfd3ec7a6b2115c2aafb5e93475c
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2023-08-23 22:13:46 +0600

    WAJP to print any message with a variable

---
+++ Example1.java
@@ -0,0 +1,10 @@
+//WAJP to print any message with a variable
+public class Example1 
+{
+    public static void main(String[] args) 
+    {
+        String sms  = "WELCOME"; //string name sms
+        System.out.println(sms);
+        
+    }
+}
