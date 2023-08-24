c81777fa1270f159fa594dceaffa87af22a69db6
Mourya <112268253+MouryaHalder@users.noreply.github.com>
2023-08-24 13:59:32 +0600

    Add files via upload
    This Java program is an example of a float data type

---
+++ Example3.java
@@ -0,0 +1,11 @@
+public class Example3
+{
+    public static void main(String[] args) {
+        int m2=10;
+        System.out.println(m2);
+        double m3=60.69;
+        System.out.println(m3);
+        double add=m2+m3;
+        System.out.println("Addition will be- "+add);
+    }
+}
